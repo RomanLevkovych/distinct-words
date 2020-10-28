0cc2a79469596cb17f0ed5ad3cfbd74cab51d24b
Roman <Levkovych>
2020-10-28 17:00:57 +0200

    Added basic threading.

--- include/words_counter.h
+++ include/words_counter.h
@@ -3,24 +3,33 @@
 
 #include <sstream>
 #include <string>
-#include <unordered_set>
 #include <string_view>
 #include <thread>
+#include <atomic>
+#include <future>
+#include <unordered_set>
 
 #include "ireader_delegate.h"
+#include "thread_safe_string_queue.h"
 
 class WordsCounter : public IReaderDelegate {
    public:
+    WordsCounter();
     // IReaderDelegate interface
     void addBuffer(std::string_view buffer) override;
     void readingFinished() override;
 
-    size_t uniqueWordsCount() const;
+    size_t uniqueWordsCount();
 
    private:
-    std::thread worker_;
+    void dataHandler();
+
     std::unordered_set<std::string> dictionary_;
     std::string container_;
+    std::thread worker_;
+    std::atomic<bool> is_alive_;
+    std::promise<size_t> count_;
+    ThreadSafeStringQueue queue_;
 };
 
 #endif  // WORDS_COUNTER_H
--- src/words_counter.cc
+++ src/words_counter.cc
@@ -1,26 +1,41 @@
 #include "words_counter.h"
 
+#include <algorithm>
+#include <iostream>
 #include <sstream>
 #include <string>
 #include <string_view>
 #include <unordered_set>
-#include <iostream>
-#include <algorithm>
+
+WordsCounter::WordsCounter() {
+    is_alive_ = true;
+    worker_ = std::thread(&WordsCounter::dataHandler, this);
+}
+
+void WordsCounter::dataHandler() {
+    static std::string word;
+    static std::string buffer;
+    do {
+        buffer = queue_.pop();
+        container_ = container_.append(buffer);
+        size_t pos;
+        while ((pos = container_.find(" ")) != std::string::npos) {
+            word = container_.substr(0, pos);
+            dictionary_.insert(word);
+            container_.erase(0, pos + 1);
+        }
+    } while (is_alive_);
+}
 
 void WordsCounter::addBuffer(std::string_view buffer) {
-    static std::string word;
-    container_ = container_.append(buffer);
-    size_t pos;
-    while ((pos = container_.find(" ")) != std::string::npos) {
-        word = container_.substr(0, pos);
-        dictionary_.insert(word);
-        container_.erase(0, pos + 1);
-    }
+    queue_.push(buffer);
 }
 
 void WordsCounter::readingFinished() {
     static std::string word;
-    size_t pos;
+    static size_t pos;
+    is_alive_ = false;
+    worker_.join();
     while ((pos = container_.find(" ")) != std::string::npos) {
         word = container_.substr(0, pos);
         dictionary_.insert(word);
@@ -30,8 +45,10 @@ void WordsCounter::readingFinished() {
         container_.erase(std::remove(container_.begin(), container_.end(), 0), container_.end());
         dictionary_.insert(container_);
     }
+    count_.set_value(dictionary_.size());
 }
 
-size_t WordsCounter::uniqueWordsCount() const {
-    return dictionary_.size();
+size_t WordsCounter::uniqueWordsCount() {
+    auto future = count_.get_future();
+    return future.get();
 }
